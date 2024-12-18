3a2d3651db90975de28c1f340ba6746f841ddf0d
Eliana <47612469+204870@users.noreply.github.com>
2024-12-18 17:41:29 -0500

    optimized the cleaning process

--- clean.sh
+++ clean.sh
@@ -22,13 +22,21 @@ rm nort
 # cat nolink | egrep http[s]*://[A-Za-z][A-Za-z]*\.[A-Za-z][A-Za-z]*/[A-Za-z0-9]* >> captured
 
 cat nolink | tr A-Z a-z >> lower
-
 rm nolink
+
+# remove hashtags but not uncide codes
+sed "s/[^\&]#//g" lower >> noht
+rm lower
+
 # [:punct:] might not be helpful since we want to preserve html unicode codes
 #cat tweets.txt | tr -d '[:punct:]' >> nopunc
-sed -e "s![␠!\"$%'()*+,-./:;<=>?@\[\]^_\`{|\}~]*!!g" lower >> nopunc
-sed -e "s/[!\"]*//" nopunc >> cleaned
-rm lower
+sed -e 's/[␠!"$%()\*+,-.:;<=>?@\[\]^_\`{|\}~]*//g' noht >> nopunc
+sed -e "s/[\/']*//g" nopunc >> cleaner
+
+rm noht
 rm nopunc
 
+# for some reason unknown to me, some things slipped through the cracks, so the rest is being cleaned with tr
+cat cleaner | tr -d "\"" | tr -d "!" | tr -d "," | tr -d "@" | tr -d "." >> cleaned
+rm cleaner
 rm tweets.txt
--- process.sh
+++ process.sh
@@ -11,13 +11,16 @@
 # file1line: label
 # file2line: tweet (cleaned + normalized)                                                                              
 echo "starting loop 1!"
-while IFS=$'\t' read -r file1line file2line; do                               
+while IFS=$'\t' read -r file1line file2line; do                            
+  echo $file1line,$file2line >> tdata_cleaned.csv
   if echo $file1line | grep -q [0-1]
   then
     # potentially capture "&#[0-9]*&# and put a space between unicharacters?
     # get them into lines and append to 'types' file
     echo $file2line | tr ' ' '\n' >> types_temp
-  fi
+    
+    # make csv-like
+  fi 
 done < <(paste "labels" "cleaned")   
 echo "loop 1 done!"
 
@@ -27,12 +30,14 @@ cat types_temp | sort | uniq >> tokens
 echo "made files"
 
 # read lines from tokens
-# cat tokens | grep ^[a-z]*$ >> tokens_alpha
-cat tokens | grep "^#[a-z]*" >> tokens_hash
+cat tokens | grep ^[a-z]*$ >> tokens_alpha
+# cat tokens | grep "^#[a-z]*" >> tokens_hash
 echo "starting loop 2!"
 while read -r line; do
   count=$(cat types_temp | grep -o "$line" | wc -l)
-  echo "$count $line" >> ordered
-done < tokens_hash
+  echo "$count $line" >> orderer
+done < tokens_alpha
+cat orderer | sort -n -r >> ordered
+rm orderer
 echo "ordered list!"
 # you can also echo type/token ratio too :)
