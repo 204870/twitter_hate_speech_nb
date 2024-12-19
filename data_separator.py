83796233c18ce7729de1a898999606d81d847e9f
Eliana <47612469+204870@users.noreply.github.com>
2024-12-18 20:14:34 -0500

    friendship ended with pandas

--- data_separator.py
+++ data_separator.py
@@ -1,31 +1,29 @@
-import os, random
+import csv, os, random
 import pandas as pd
 
 # this script separates the training data and the test data
 
 ## read from csv and get length
-df = pd.read_csv('tdata.csv') # i will instead read the csv i make after cleaning the data where i have 'index,class,tweet' as the indices
-tdata_length = df.shape[0]
+with open('tdata_cleaned.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)[1:]
+tdata_length = len(data)
 
 # get 'random' sample of 5000 to use as training data using a starting seed
 sample_size = 5000
 initdex = random.randint(0, tdata_length)
 
-df_train = pd.DataFrame(columns=df.columns)
+data_train = []
 
-txt = 'index,class,tweet'
 for i in range(initdex, (initdex+sample_size)):
   j = i-initdex
   # ensure it can wrap depending on the starting seed
   if (i >= tdata_length):
     i = i % tdata_length
-  df_train.loc[j] = df.loc[i]
+  data_train.append(data.pop(i))
 
-# i apparently have to make a second for loop for this part, maybe i should've done this with numpy instead of pandas
-for i in range(initdex, (initdex+sample_size)):
-  if (i >= tdata_length):
-    i = i % tdata_length
-  df.drop(i)
+df = pd.DataFrame(data, columns=['class', 'tweet'])
+df_train = pd.DataFrame(data_train, columns=['class', 'tweet'])
 
 # write training and test data to parquet because csv doesn't work because the tweets have commas :'(
 df_train.to_parquet('training.parquet')
