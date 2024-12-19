ee668f17f99b547e19b2cb24fec48dea89ffb4a1
Eliana <47612469+204870@users.noreply.github.com>
2024-12-19 01:02:45 -0500

    Update texas.py

--- texas.py
+++ texas.py
@@ -1,8 +1,8 @@
-import os, pyarrow
+import os
 import pandas as pd
 
 df = pd.read_parquet("train-00000-of-00001.parquet")
-''' 
+'''
 ~~~~~~~~Data Fields~~~~~~~~~~~~~~~~~~~~~~~~~
 count: (Integer) number of users who coded each tweet (min is 3, sometimes more users coded a tweet when judgments were determined to be unreliable,
 hate_speech_annotation: (Integer) number of users who judged the tweet to be hate speech,
@@ -11,6 +11,16 @@ neither_annotation: (Integer) number of users who judged the tweet to be neither
 label: (Class Label) class label for majority of CF users (0: 'hate-speech', 1: 'offensive-language' or 2: 'neither'),
 tweet: (string)
 '''
+# forgive me for i could not find a good way to do this in bash
+# believe me, i've done a fair amount of searching in my opinion but
+# most of the tools we've been introduced to work on a line-by-line basis and well..
+# some tweets have newline chars in them :')
+l = df.shape[0]
+li = df.tweet.tolist()
+for i in range(0,l):
+  li[i].replace("\n", "")
+df.tweet = li
+
 df.to_csv('tdata.csv')
-df['class'].to_csv('labels.csv')
 df.tweet.to_csv('tweets.csv')
+df['class'].to_csv('labels.csv')
